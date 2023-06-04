4691412199250e18b28992429b29abfc747324ca
Mykhailo Rogozhyn <imihanick@gmail.com>
2023-06-04 21:29:09 +0300

    Version 1.9

--- comparator/comparator.py
+++ comparator/comparator.py
@@ -1,41 +1,55 @@
 import os
+from datetime import datetime
 
 def make_list_without(main_type = "idw", slave_type = "pdf"):
     # Makes list with main files, which have not pairs of slave files
-    print("\n{0} files without {1} files:\n---------------------------------------".format(main_type, slave_type))
+    print("\n{0} files without {1} files:\n{2}".format(main_type, slave_type, separator), file = file)
     if main_type and slave_type in extensions_dict:
         main_without_slave_list = sorted(list(set(extensions_dict[main_type]) - set(extensions_dict[slave_type])))
-        for number, name in enumerate(main_without_slave_list):
-            print("{0:>3} : {1}".format(number+1, name))
-        print("---------------------------------------\n")
-        return {"main type" : main_type, "slave type" : slave_type, "operation" : "main without slave", "list" : main_without_slave_list}
+        if main_without_slave_list == []:
+            print("Each {0} file has {1} file\n\n{2}".format(main_type, slave_type, separator), file = file)
+        else:
+            for number, name in enumerate(main_without_slave_list):
+                print("{0:>3} : {1}".format(number+1, name), file = file)
+            print("\n", separator, "\n", file = file)
+            return {"main type" : main_type,
+                "slave type" : slave_type,
+                    "operation" : "main without slave",
+                    "list" : main_without_slave_list}
     else:
         if main_type not in extensions_dict:
-            print ("There are not {0} files".format(main_type))
+            print ("There are not {0} files".format(main_type), file = file)
         if slave_type not in extensions_dict:
-            print ("There are not {0} files".format(slave_type))
-        print("---------------------------------------\n")
+            print ("There are not {0} files".format(slave_type), file = file)
+        print("\n", separator, "\n", file = file)
 
 def make_dict_with (main_type = "idw", slave_type = "pdf"):
     # Makes dictionary with main files, which have pairs of slave files
-    print("\n{0} files with {1} files:\n---------------------------------------".format(main_type, slave_type))
+    print("\n{0} files with {1} files:\n{2}".format(main_type, slave_type, separator), file = file)
     if main_type and slave_type in extensions_dict:
         main_with_slave_dict = {}
         for number, name in enumerate(sorted(list(set(extensions_dict[main_type]) & set(extensions_dict[slave_type])))):
-            main_with_slave_dict[name] = round((names_dict[name + "." + main_type]["File mod. time"] - names_dict[name + "." + slave_type]["File mod. time"])/60)
-            print("{0:>3} : {1:<20} : {2:>5} minutes".format(number+1, name, main_with_slave_dict[name]))
-        print("---------------------------------------\n")
-        return {"main type" : main_type, "slave type" : slave_type, "operation" : "main with slave", "dictionary" : main_with_slave_dict}
+            main_with_slave_dict[name] = round((names_dict[name + "." + main_type]["File mod. time"] - 
+                                                names_dict[name + "." + slave_type]["File mod. time"])/60)
+            print("{0:>3} : {1:<20} : {2:>5} minutes".format(number+1, name, main_with_slave_dict[name]), file = file)
+        print("\n", separator, "\n", file = file)
+        return {"main type" : main_type,
+                "slave type" : slave_type,
+                "operation" : "main with slave",
+                "dictionary" : main_with_slave_dict}
     else:
         if main_type not in extensions_dict:
-            print ("There are not {0} files".format(main_type))
+            print ("There are not {0} files".format(main_type), file = file)
         if slave_type not in extensions_dict:
-            print ("There are not {0} files".format(slave_type))
-        print("---------------------------------------\n")
+            print ("There are not {0} files".format(slave_type), file = file)
+        print(separator, "\n", file = file)
 
 cur_path = os.getcwd()
-
-print("Curren directory:\n", cur_path,)
+cur_date = datetime.now().date()
+cur_time = datetime.now().time()
+cur_date_time = str(cur_date) + "_" + "{:0>2}".format(str(cur_time.hour)) +"{:0>2}".format(str(cur_time.minute))
+file_name = "Result_" + cur_date_time + ".txt"
+separator = "-----------------------------------------\n"
 
 # Makes dictionary with all files information
 names_dict = {}
@@ -59,9 +73,17 @@ for key in extensions_dict:
         if names_dict[name]["File extension"] == key:
             extensions_dict[key].append(names_dict[name]["File name"])
 
+file = open(file_name, "w")
+
+print("Date: {0}\nTime: {1:0>2}:{2:0>2}".format(str(cur_date),str(cur_time.hour), str(cur_time.minute)), file = file)
+print("Curren directory: {}\n".format(cur_path), file = file)
+
 idw_without_pdf = make_list_without()
 idw_with_pdf = make_dict_with()
 idw_without_dwg = make_list_without("idw", "dwg")
 idw_with_dwg = make_dict_with("idw", "dwg")
 
-input('')
+file.close()
+
+print('The result you can find in the file "{}" in the current directory'.format(file_name))
+input('\nPress Enter')
