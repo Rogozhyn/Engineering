29e64ec89d7cebcc911f06c5d3f6aa36d7ac0579
Mykhailo Rogozhyn <imihanick@gmail.com>
2023-06-04 21:52:29 +0300

    Version 1.0

---
+++ logs_backup/logs_backup.py
@@ -0,0 +1,25 @@
+import os
+import datetime
+import zipfile
+
+archives = ('U:\\Users\\mrogozhyn\\Desktop\\Logs backups',
+            'Q:\\01_Backup\\Logs backups')
+
+logs = {'0103': ('W:\\03_Workspace\\0103-00-000.00_Книга учета времени.ods',
+                 (os.path.join(archives[0], '0103-00-000.00'), os.path.join(archives[1], '0103-00-000.00'))),
+        '0104': ('W:\\03_Workspace Pavalex\\0104-00-000.00_Книга учета времени.ods',
+                 (os.path.join(archives[0], '0104-00-000.00'), os.path.join(archives[1], '0104-00-000.00')))
+        }
+
+year_string = datetime.date.today().strftime("%Y")
+month_string = datetime.date.today().strftime("%Y-%m")
+date_string = datetime.date.today().strftime("%Y-%m-%d")
+
+for log in logs:
+    for path in logs[log][1]:
+        dir_path = os.path.join(path, year_string, month_string)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        zip_file_path = os.path.join(dir_path, date_string + '.zip')
+        zip_file = zipfile.ZipFile(str(zip_file_path), 'w')
+        zip_file.write(str(logs[log][0]))
