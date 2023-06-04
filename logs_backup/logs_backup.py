7e1c1490f3a27fad6b46817af4d6c721de80278d
Mykhailo Rogozhyn <imihanick@gmail.com>
2023-06-04 21:53:16 +0300

    Version 1.2

--- logs_backup/logs_backup.py
+++ logs_backup/logs_backup.py
@@ -5,9 +5,9 @@ import zipfile
 archives = ('U:\\Users\\mrogozhyn\\Desktop\\Logs backups',
             'Q:\\01_Backup\\Logs backups')
 
-logs = {'0103': ('W:\\03_Workspace\\0103-00-000.00_Книга учета времени.ods', '0103-00-000.00'),
-        '0104': ('W:\\03_Workspace Pavalex\\0104-00-000.00_Книга учета времени.ods', '0104-00-000.00')
-        }
+logs = (('0103-00-000.00', 'W:\\03_Workspace\\0103-00-000.00_Книга учета времени.ods'),
+        ('0104-00-000.00', 'W:\\03_Workspace Pavalex\\0104-00-000.00_Книга учета времени.ods')
+        )
 
 year_string = datetime.date.today().strftime("%Y")
 month_string = datetime.date.today().strftime("%Y-%m")
@@ -15,9 +15,9 @@ date_string = datetime.date.today().strftime("%Y-%m-%d")
 
 for log in logs:
     for path in archives:
-        dir_path = os.path.join(path, logs[log][1], year_string, month_string)
+        dir_path = os.path.join(path, log[0], year_string, month_string)
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
         zip_file_path = os.path.join(dir_path, date_string + '.zip')
         zip_file = zipfile.ZipFile(str(zip_file_path), 'w')
-        zip_file.write(str(logs[log][0]))
+        zip_file.write(str(log[1]))
