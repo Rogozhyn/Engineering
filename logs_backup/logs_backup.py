5776036a0f8be9ba549f3d9068a38b15a53266a1
Mykhailo Rogozhyn <imihanick@gmail.com>
2023-06-04 21:54:24 +0300

    Version 1.3.2

--- logs_backup/logs_backup.py
+++ logs_backup/logs_backup.py
@@ -2,7 +2,7 @@ import os
 import datetime
 import zipfile
 
-archives = ('D:\\Users\\Mykhailo Rogozhyn\\Desktop\\Logs backups',
+archives = ('U:\\Users\\mrogozhyn\\Desktop\\Logs backups',
             'Q:\\01_Backup\\Logs backups',
             'Q:\\02_Google drive\\03_imihanick\\ФОП Рогожин\\02_Logs backups')
 
@@ -12,15 +12,18 @@ logs = (('0103-00-000.00', 'W:\\03_Workspace', '0103-00-000.00_Книга уче
         )
 
 year_string = datetime.date.today().strftime("%Y")
-month_string = datetime.date.today().strftime("%Y-%m")
-date_string = datetime.date.today().strftime("%Y-%m-%d")
+month_string = datetime.date.today().strftime("%m")
+date_string = datetime.date.today().strftime("%d")
+cur_time = datetime.datetime.now().time()
+time_string = "{0:0>2}-{1:0>2}".format(str(cur_time.hour), str(cur_time.minute))
+
 
 for log in logs:
     os.chdir(log[1])
     for path in archives:
-        dir_path = os.path.join(path, log[0], year_string, month_string)
+        dir_path = os.path.join(path, log[0], year_string, month_string, date_string)
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
-        zip_file_path = os.path.join(dir_path, date_string + '.zip')
+        zip_file_path = os.path.join(dir_path, time_string + '.zip')
         zip_file = zipfile.ZipFile(str(zip_file_path), 'w')
         zip_file.write(str(log[2]))
